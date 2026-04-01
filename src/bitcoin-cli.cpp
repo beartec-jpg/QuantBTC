9bec242508518bfc21b2abc4a3ea6066c031e29a
scott reynolds <beartec@beartec.uk>
2026-04-01 08:18:13 +0000

    feat: Update references from Bitcoin to QuantumBTC in CLI and utility files

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -145,14 +145,14 @@ static int AppInitRPC(int argc, char* argv[])
         return EXIT_FAILURE;
     }
     if (argc < 2 || HelpRequested(gArgs) || gArgs.IsArgSet("-version")) {
-        std::string strUsage = PACKAGE_NAME " RPC client version " + FormatFullVersion() + "\n";
+        std::string strUsage = "QuantumBTC RPC client version " + FormatFullVersion() + "\n";
 
         if (gArgs.IsArgSet("-version")) {
             strUsage += FormatParagraph(LicenseInfo());
         } else {
             strUsage += "\n"
-                "Usage:  quantumbtc-cli [options] <command> [params]  Send command to " PACKAGE_NAME "\n"
-                "or:     quantumbtc-cli [options] -named <command> [name=value]...  Send command to " PACKAGE_NAME " (with named arguments)\n"
+                "Usage:  quantumbtc-cli [options] <command> [params]  Send command to QuantumBTC\n"
+                "or:     quantumbtc-cli [options] -named <command> [name=value]...  Send command to QuantumBTC (with named arguments)\n"
                 "or:     quantumbtc-cli [options] help                List commands\n"
                 "or:     quantumbtc-cli [options] help <command>      Get help for a command\n";
             strUsage += "\n" + gArgs.GetHelpMessage();
@@ -535,7 +535,7 @@ public:
         }
 
         // Generate report header.
-        std::string result{strprintf("%s client %s%s - server %i%s\n\n", PACKAGE_NAME, FormatFullVersion(), ChainToString(), networkinfo["protocolversion"].getInt<int>(), networkinfo["subversion"].get_str())};
+        std::string result{strprintf("%s client %s%s - server %i%s\n\n", "QuantumBTC", FormatFullVersion(), ChainToString(), networkinfo["protocolversion"].getInt<int>(), networkinfo["subversion"].get_str())};
 
         // Report detailed peer connections list sorted by direction and minimum ping time.
         if (DetailsRequested() && !m_peers.empty()) {
